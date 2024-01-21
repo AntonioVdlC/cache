a3630e0b0058973266a980415c5435f833305f59
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2024-01-21 17:07:53 +0800

    Implement LRU cache

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,273 @@
-export default function add(a: number, b: number) {
-  return a + b;
+/**
+ * A simple LRU cache implementation
+ * @class LRUCache
+ *
+ * @template K - The key type
+ * @template V - The value type
+ *
+ * @method get - Gets the value associated with the key
+ * @method put - Puts the key-value pair into the cache
+ * @method peek - Gets the value associated with the key without updating the cache
+ * @method has - Checks if the key exists in the cache
+ * @method remove - Removes the key-value pair from the cache
+ * @method clear - Clears the cache
+ * @method resize - Resizes the cache
+ *
+ * @getter first - Gets the first key in the cache
+ * @getter last - Gets the last key in the cache
+ * @getter size - Gets the number of items in the cache
+ * @getter capacity - Gets the maximum number of items the cache can hold
+ *
+ * @throws {Error} - If capacity is less than 1
+ *
+ * @example
+ * const cache = new LRUCache<string, number>(2);
+ * cache.put("a", 1);
+ * cache.put("b", 2);
+ * cache.get("a"); // 1
+ * cache.put("c", 3);
+ * cache.get("b"); // undefined
+ *
+ */
+class LRUCache<K, V> {
+  /**
+   * @param {number} capacity - The maximum number of items the cache can hold
+   */
+  #capacity: number;
+
+  /**
+   * @param {Map<K, V>} map - The underlying map that holds the cache
+   */
+  #map: Map<K, V>;
+
+  /**
+   * Creates a new LRUCache
+   *
+   * @param {number} capacity - The maximum number of items the cache can hold
+   *
+   * @throws {Error} - If capacity is less than 1
+   *
+   * @example
+   * const cache = new LRUCache<string, number>(2);
+   * cache.capacity; // 2
+   */
+  constructor(capacity: number) {
+    if (capacity < 1) {
+      throw new Error("Capacity must be greater than 0");
+    }
+
+    this.#capacity = capacity;
+    this.#map = new Map();
+  }
+
+  /**
+   * Gets the value associated with the key, and updates the cache to reflect
+   * the most recently used item (if it exists)
+   *
+   * @param {K} key - The key to get the value for
+   *
+   * @returns {V | undefined} - The value associated with the key
+   *
+   * @example
+   * const cache = new LRUCache<string, number>(2);
+   * cache.put("a", 1);
+   * cache.get("a"); // 1
+   * cache.get("b"); // undefined
+   */
+  get(key: K): V | undefined {
+    const value = this.#map.get(key);
+    if (value) {
+      this.#map.delete(key);
+      this.#map.set(key, value);
+
+      return value;
+    }
+  }
+
+  /**
+   * Puts the key-value pair into the cache, and evicts the least recently used
+   * item if the cache is full
+   *
+   * @param {K} key - The key to put the value for
+   * @param {V} value - The value to put for the key
+   *
+   * @example
+   * const cache = new LRUCache<string, number>(2);
+   * cache.put("a", 1);
+   * cache.put("b", 2);
+   * cache.put("c", 3);
+   * cache.get("a"); // undefined
+   * cache.get("b"); // 2
+   * cache.get("c"); // 3
+   */
+  put(key: K, value: V): void {
+    if (this.#map.has(key)) {
+      this.#map.delete(key);
+    } else if (this.size === this.capacity) {
+      this.#map.delete(this.first!);
+    }
+
+    this.#map.set(key, value);
+  }
+
+  /**
+   * Gets the value associated with the key without updating the cache
+   *
+   * @param {K} key - The key to peek the value for
+   *
+   * @returns {V | undefined} - The value associated with the key
+   *
+   * @example
+   * const cache = new LRUCache<string, number>(2);
+   * cache.put("a", 1);
+   * cache.put("b", 2);
+   * cache.peek("a"); // 1
+   * cache.peek("c"); // undefined
+   */
+  peek(key: K): V | undefined {
+    return this.#map.get(key);
+  }
+
+  /**
+   * Checks if the key exists in the cache without updating the cache
+   *
+   * @param {K} key - The key to check for
+   *
+   * @returns {boolean} - True if the key exists in the cache, false otherwise
+   *
+   * @example
+   * const cache = new LRUCache<string, number>(2);
+   * cache.put("a", 1);
+   * cache.put("b", 2);
+   * cache.has("a"); // true
+   * cache.has("c"); // false
+   */
+  has(key: K): boolean {
+    return this.#map.has(key);
+  }
+
+  /**
+   * Removes the key-value pair from the cache if it exists
+   *
+   * @param {K} key - The key to remove
+   *
+   * @example
+   * const cache = new LRUCache<string, number>(2);
+   * cache.put("a", 1);
+   * cache.put("b", 2);
+   * cache.remove("a");
+   * cache.has("a"); // false
+   * cache.has("b"); // true
+   * cache.size; // 1
+   */
+  remove(key: K): void {
+    this.#map.delete(key);
+  }
+
+  /**
+   * Clears the cache of all key-value pairs
+   *
+   * @example
+   * const cache = new LRUCache<string, number>(2);
+   * cache.put("a", 1);
+   * cache.put("b", 2);
+   * cache.clear();
+   * cache.has("a"); // false
+   * cache.has("b"); // false
+   * cache.size; // 0
+   */
+  clear(): void {
+    this.#map.clear();
+  }
+
+  /**
+   * Resizes the cache to the new capacity if the new capacity is greater than
+   * 0. If the new capacity is less than the current size of the cache, the
+   * least recently used items will be evicted until the size of the cache is
+   * equal to the new capacity.
+   *
+   * @param {number} capacity - The new capacity of the cache
+   *
+   * @throws {Error} - If capacity is less than 1
+   *
+   * @example
+   * const cache = new LRUCache<string, number>(2);
+   * cache.put("a", 1);
+   * cache.put("b", 2);
+   * cache.resize(1);
+   * cache.has("a"); // false
+   * cache.has("b"); // true
+   * cache.size; // 1
+   * cache.capacity; // 1
+   */
+  resize(capacity: number): void {
+    if (capacity < 1) {
+      throw new Error("Capacity must be greater than 0");
+    }
+
+    this.#capacity = capacity;
+
+    while (this.size > this.capacity) {
+      this.#map.delete(this.first!);
+    }
+  }
+
+  /**
+   * Gets the first key in the cache
+   *
+   * @returns {K | undefined} - The first key in the cache
+   *
+   * @example
+   * const cache = new LRUCache<string, number>(2);
+   * cache.put("a", 1);
+   * cache.put("b", 2);
+   * cache.first; // "a"
+   */
+  get first(): K | undefined {
+    return this.#map.keys().next().value;
+  }
+
+  /**
+   * Gets the last key in the cache
+   *
+   * @returns {K | undefined} - The last key in the cache
+   *
+   * @example
+   * const cache = new LRUCache<string, number>(2);
+   * cache.put("a", 1);
+   * cache.put("b", 2);
+   * cache.last; // "b"
+   */
+  get last(): K | undefined {
+    return Array.from(this.#map.keys()).pop();
+  }
+
+  /**
+   * Gets the number of items in the cache
+   *
+   * @returns {number} - The number of items in the cache
+   *
+   * @example
+   * const cache = new LRUCache<string, number>(1);
+   * cache.put("a", 1);
+   * cache.size; // 1
+   */
+  get size(): number {
+    return this.#map.size;
+  }
+
+  /**
+   * Gets the maximum number of items the cache can hold
+   *
+   * @returns {number} - The maximum number of items the cache can hold
+   *
+   * @example
+   * const cache = new LRUCache<string, number>(1);
+   * cache.capacity; // 1
+   */
+  get capacity(): number {
+    return this.#capacity;
+  }
 }
+
+export default LRUCache;
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,16 +1,243 @@
 import { describe, it, expect } from "vitest";
 
-import add from "../src";
+import LRUCache from "../src";
 
-describe("add", () => {
-  it("is a function", () => {
-    expect(typeof add).toBe("function");
+describe("LRUCache", () => {
+  describe("constructor", () => {
+    it("should be a class", () => {
+      expect(typeof LRUCache).toBe("function");
+    });
+    it("should be instantiable", () => {
+      const cache = new LRUCache<string, number>(1);
+      expect(cache).toBeInstanceOf(LRUCache);
+    });
+    it("should throw an error if capacity is less than 1", () => {
+      expect(() => new LRUCache<string, number>(0)).toThrow(
+        "Capacity must be greater than 0",
+      );
+    });
   });
 
-  it("adds 2 numbers", () => {
-    const a = 1234;
-    const b = 4321;
+  describe("getters", () => {
+    describe("first", () => {
+      it("should return the first key in the cache", () => {
+        const cache = new LRUCache<string, number>(2);
+        cache.put("a", 1);
+        cache.put("b", 2);
+        expect(cache.first).toBe("a");
+      });
+      it("should return undefined if cache is empty", () => {
+        const cache = new LRUCache<string, number>(1);
+        expect(cache.first).toBe(undefined);
+      });
+    });
 
-    expect(add(a, b)).toBe(a + b);
+    describe("last", () => {
+      it("should return the last key in the cache", () => {
+        const cache = new LRUCache<string, number>(2);
+        cache.put("a", 1);
+        cache.put("b", 2);
+        expect(cache.last).toBe("b");
+      });
+      it("should return undefined if cache is empty", () => {
+        const cache = new LRUCache<string, number>(1);
+        expect(cache.last).toBe(undefined);
+      });
+    });
+
+    describe("size", () => {
+      it("should return the size of the cache (empty)", () => {
+        const cache = new LRUCache<string, number>(1);
+        expect(cache.size).toBe(0);
+      });
+      it("should return the size of the cache", () => {
+        const cache = new LRUCache<string, number>(1);
+        cache.put("a", 1);
+        expect(cache.size).toBe(1);
+      });
+    });
+
+    describe("capacity", () => {
+      it("should return the capacity of the cache", () => {
+        const cache = new LRUCache<string, number>(1);
+        expect(cache.capacity).toBe(1);
+      });
+    });
+  });
+
+  describe("methods", () => {
+    describe("get", () => {
+      it("should return undefined if key does not exist", () => {
+        const cache = new LRUCache<string, number>(1);
+        expect(cache.get("a")).toBe(undefined);
+      });
+
+      it("should return the value if key exists", () => {
+        const cache = new LRUCache<string, number>(1);
+        cache.put("a", 1);
+        expect(cache.get("a")).toBe(1);
+      });
+
+      it("should move the key to the end of the cache", () => {
+        const cache = new LRUCache<string, number>(2);
+        cache.put("a", 1);
+        cache.put("b", 2);
+        expect(cache.first).toBe("a");
+        cache.get("a");
+        expect(cache.last).toBe("a");
+      });
+
+      it("shouldn't move the key if element is not in the cache", () => {
+        const cache = new LRUCache<string, number>(2);
+        cache.put("a", 1);
+        cache.put("b", 2);
+        expect(cache.first).toBe("a");
+        cache.get("c");
+        expect(cache.first).toBe("a");
+      });
+    });
+
+    describe("put", () => {
+      it("should add a new key/value pair to the cache", () => {
+        const cache = new LRUCache<string, number>(1);
+        cache.put("a", 1);
+        expect(cache.size).toBe(1);
+        expect(cache.get("a")).toBe(1);
+      });
+
+      it("should update the value of an existing key", () => {
+        const cache = new LRUCache<string, number>(1);
+        cache.put("a", 1);
+        expect(cache.size).toBe(1);
+        expect(cache.get("a")).toBe(1);
+        cache.put("a", 2);
+        expect(cache.size).toBe(1);
+        expect(cache.get("a")).toBe(2);
+      });
+
+      it("should move the key to the end of the cache", () => {
+        const cache = new LRUCache<string, number>(2);
+        cache.put("a", 1);
+        cache.put("b", 2);
+        expect(cache.first).toBe("a");
+        cache.put("a", 1);
+        expect(cache.last).toBe("a");
+      });
+
+      it("should remove the least recently used key if cache is full", () => {
+        const cache = new LRUCache<string, number>(1);
+        cache.put("a", 1);
+        cache.put("b", 2);
+        expect(cache.get("a")).toBe(undefined);
+      });
+    });
+
+    describe("peek", () => {
+      it("should return undefined if key does not exist", () => {
+        const cache = new LRUCache<string, number>(1);
+        expect(cache.peek("a")).toBe(undefined);
+      });
+
+      it("should return the value if key exists", () => {
+        const cache = new LRUCache<string, number>(1);
+        cache.put("a", 1);
+        expect(cache.peek("a")).toBe(1);
+      });
+
+      it("should not move the key to the end of the cache", () => {
+        const cache = new LRUCache<string, number>(2);
+        cache.put("a", 1);
+        cache.put("b", 2);
+        expect(cache.first).toBe("a");
+        cache.peek("a");
+        expect(cache.first).toBe("a");
+      });
+    });
+
+    describe("has", () => {
+      it("should return false if key does not exist", () => {
+        const cache = new LRUCache<string, number>(1);
+        expect(cache.has("a")).toBe(false);
+      });
+
+      it("should return true if key exists", () => {
+        const cache = new LRUCache<string, number>(1);
+        cache.put("a", 1);
+        expect(cache.has("a")).toBe(true);
+      });
+
+      it("should not move the key to the end of the cache", () => {
+        const cache = new LRUCache<string, number>(2);
+        cache.put("a", 1);
+        cache.put("b", 2);
+        expect(cache.first).toBe("a");
+        cache.has("a");
+        expect(cache.first).toBe("a");
+      });
+    });
+
+    describe("remove", () => {
+      it("should remove the key from the cache", () => {
+        const cache = new LRUCache<string, number>(1);
+        cache.put("a", 1);
+        expect(cache.has("a")).toBe(true);
+        expect(cache.size).toBe(1);
+        cache.remove("a");
+        expect(cache.has("a")).toBe(false);
+        expect(cache.size).toBe(0);
+      });
+
+      it("should not throw an error if key does not exist", () => {
+        const cache = new LRUCache<string, number>(1);
+        expect(() => cache.remove("a")).not.toThrow();
+      });
+    });
+
+    describe("clear", () => {
+      it("should clear the cache", () => {
+        const cache = new LRUCache<string, number>(2);
+        cache.put("a", 1);
+        cache.put("b", 2);
+        expect(cache.size).toBe(2);
+        cache.clear();
+        expect(cache.has("a")).toBe(false);
+        expect(cache.has("b")).toBe(false);
+        expect(cache.size).toBe(0);
+        expect(cache.capacity).toBe(2);
+      });
+    });
+
+    describe("resize", () => {
+      it("should update the capacity of the cache", () => {
+        const cache = new LRUCache<string, number>(1);
+        expect(cache.capacity).toBe(1);
+        cache.resize(2);
+        expect(cache.capacity).toBe(2);
+      });
+      it("should throw an error if capacity is less than 1", () => {
+        const cache = new LRUCache<string, number>(1);
+        expect(() => cache.resize(0)).toThrow(
+          "Capacity must be greater than 0",
+        );
+      });
+      it("should remove the least recently used keys if capacity is less than size", () => {
+        const cache = new LRUCache<string, number>(3);
+        cache.put("a", 1);
+        cache.put("b", 2);
+        cache.put("c", 3);
+        cache.resize(1);
+        expect(cache.has("a")).toBe(false);
+        expect(cache.has("b")).toBe(false);
+        expect(cache.has("c")).toBe(true);
+      });
+      it("should not remove the least recently used keys if capacity is greater than size", () => {
+        const cache = new LRUCache<string, number>(2);
+        cache.put("a", 1);
+        cache.put("b", 2);
+        cache.resize(3);
+        expect(cache.has("a")).toBe(true);
+        expect(cache.has("b")).toBe(true);
+      });
+    });
   });
 });
